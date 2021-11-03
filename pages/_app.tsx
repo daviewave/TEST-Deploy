96a778b83680b5ff02d43589eeb10636c132a7c1
Stephen Bonish <sbonish3535@gmail.com>
2021-11-02 21:15:23 -0400

    adding some of wallet connection stuff

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,9 @@
-import '../styles/globals.css'
+import { WalletModalProvider } from '@solana/wallet-adapter-react-ui';
+import dynamic from 'next/dynamic';
+import { FC, ReactNode } from 'react';
+require('../styles/globals.css');
+require('@solana/wallet-adapter-react-ui/styles.css');
+
 
 //bootstrap import
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -6,8 +11,24 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 import type { AppProps } from 'next/app'
 
+const WalletConnect = dynamic<{ children: ReactNode }>(
+    () =>
+        import('../components/walletConnect').then(
+            ({ WalletConnectionProvider }) => WalletConnectionProvider
+        ),
+    {
+        ssr: false,
+    }
+);
+
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+  return (
+  		<WalletConnect>
+            <WalletModalProvider>
+                <Component {...pageProps} />
+            </WalletModalProvider>
+        </WalletConnect>
+    );
+};
 
 export default MyApp
